bfeb21e6e0ab36f544926e0cf3b00717f06f2274
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-04-24 22:04:01 -0300

    hypr: fix wl-copy screenshot

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -2,4 +2,4 @@
 
 temp_screenshot="/tmp/screenshot.png"
 
-grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot | wl-copy
+grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot && wl-copy
