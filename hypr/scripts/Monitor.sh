ec72541c1593f4687e38f4da0807e305aa302b11
Eduardo Rizzo <eduardorizzo1@outlook.com>
2025-08-16 17:15:25 +1000

    hypr: check monitors

--- hypr/scripts/Monitor.sh
+++ hypr/scripts/Monitor.sh
@@ -1,2 +1,11 @@
-# List monitors
-hyprctl monitors | grep 'Monitor' | head -n1 | awk '{print $2}'
+#!/bin/bash
+
+sleep 3
+
+# Compare monitors
+actualMonitor=$(hyprctl monitors | grep 'Monitor' | head -n1 | awk '{print $2}')
+fileConfigMonitor=$(grep -m1 'monitor=' ~/.config/hypr/monitors.conf | cut -d= -f2- | cut -d, -f1)
+
+if [ "$actualMonitor" != "$fileConfigMonitor" ]; then
+	notify-send "   Monitor: The current monitor is different from the config file"
+fi
