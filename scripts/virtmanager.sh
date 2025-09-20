dfa2bebd0743435fc958f6692602933f4164f167
Eduardo Rizzo <eduardorizzo1@outlook.com>
2025-09-20 20:43:14 +1000

    scripts: add virtd

---
+++ scripts/virtmanager.sh
@@ -0,0 +1,5 @@
+sudo pacman -S qemu-desktop libvirt virt-manager dnsmasq iptables-nft ovmf bridge-utils
+
+# sudo systemctl enable --now virtlogd.socket virtlockd.socket libvirtd.service
+# sudo usermod -aG libvirt $USER
+# newgrp libvirt
